2e586ad7709e7d9a8bf326fe82eed6981212bcd1
AABakotin <megabart57@gmail.com>
2021-09-25 20:12:54 +0300

    Box

---
+++ src/main/java/Generics/ChangeToArray.java
@@ -0,0 +1,17 @@
+package Generics;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class ChangeToArray {
+
+    public static ArrayList<Object> toArray(Object[] arr) {
+
+        return new ArrayList<>(Arrays.asList(arr));   //Идея сама преобразовала в одну строчку
+
+        //      ArrayList<Object> arrayList = new ArrayList<>(Arrays.asList(arr));
+        //       return arrayList;
+
+
+    }
+}
